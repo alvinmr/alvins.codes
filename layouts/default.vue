ec78784bbe67cd79c0a2c8aa79bc49b1c29bcf9b
alvinmr <maulanaalvin1@gmail.com>
2020-06-09 16:53:59 +0800

    fix transtition

--- layouts/default.vue
+++ layouts/default.vue
@@ -40,6 +40,16 @@
 
 
 <style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+
+.page-enter,
+.page-leave-active {
+  opacity: 0;
+}
+
 @font-face {
   font-family: "Brandon";
   src: url("~assets/fonts/Brandon_reg.otf");
