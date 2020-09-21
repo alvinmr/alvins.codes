13893cc6cae7f133bab6bc0550dfb33927159961
alvinmr <maulanaalvin1@gmail.com>
2020-09-21 14:23:07 +0800

    change size font

--- components/global/BlockSquare.vue
+++ components/global/BlockSquare.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="p-4 my-10 font-bold bg-white border-2 border-black rounded-lg shadow-theme">
+  <div
+    class="p-4 my-10 text-xs font-bold text-left bg-white border-2 border-black rounded-lg lg:text-base shadow-theme"
+  >
     <slot name="text">default</slot>
   </div>
 </template>
