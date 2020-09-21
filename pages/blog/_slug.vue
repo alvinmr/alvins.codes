0f02f795600e18469fc14cffc9645fc4967c357d
alvinmr <maulanaalvin1@gmail.com>
2020-09-21 13:39:17 +0800

    adding meta tag SEO

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -3,7 +3,10 @@
     <nuxt-link class="px-4 py-2 text-white bg-black" to="/blog" exact>Back To Blog</nuxt-link>
     <Header :title="article.title" />
 
-    <nuxt-content class="mx-auto mt-24 prose sm:prose lg:prose-lg xl:prose-xl" :document="article" />
+    <nuxt-content
+      class="mx-auto mt-24 prose text-justify sm:prose lg:prose-lg xl:prose-xl"
+      :document="article"
+    />
   </div>
 </template>
 
@@ -11,6 +14,29 @@
 import Header from "~/components/Header.vue";
 export default {
   components: { Header },
+  head() {
+    return {
+      title: `alvinscodes | ${this.article.title}`,
+      meta: [
+        { hid: "og:title", name: "og:title", content: this.article.title },
+        {
+          hid: "og:image",
+          name: "og:image",
+          content: this.article.header_image,
+        },
+        {
+          hid: "og:description",
+          name: "og:description",
+          content: this.article.description,
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: this.article.description,
+        },
+      ],
+    };
+  },
   async asyncData({ $content, params }) {
     const article = await $content("blog", params.slug).fetch();
     return { article };
