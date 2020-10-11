5bf3a9ed2976a3d384f467df92179c7d03637db1
alvinmr <maulanaalvin1@gmail.com>
2020-10-12 01:46:37 +0800

    ux loading contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,8 +4,14 @@
       <div class="grid justify-center mt-4 text-center h-80 lg:text-left">
         <h1
           class="w-full text-3xl lg:w-2/3 lg:text-6xl md:text-6xl font-brandonBlack"
-        >Let's work together</h1>
-        <img src="~assets/images/cont2.gif" class="mx-auto lg:mx-px" alt="contact me" />
+        >
+          Let's work together
+        </h1>
+        <img
+          src="~assets/images/cont2.gif"
+          class="mx-auto lg:mx-px"
+          alt="contact me"
+        />
       </div>
       <div
         class="p-3 mt-10 bg-white border-2 border-black lg:w-3/4 project-wrap shadow-theme rounded-theme"
@@ -28,17 +34,29 @@
             <label for="service">Service</label>
             <select v-model="service" class="input-group" name="service">
               <option value disabled>Choose what you needed</option>
-              <option value="Just wanna say hello!">Just wanna say hello!</option>
-              <option value="Need help with a project">Need help with a project</option>
-              <option value="Long term partnership">Long term partnership</option>
+              <option value="Just wanna say hello!">
+                Just wanna say hello!
+              </option>
+              <option value="Need help with a project">
+                Need help with a project
+              </option>
+              <option value="Long term partnership">
+                Long term partnership
+              </option>
               <option value="Hire me full-time">Hire me full-time</option>
             </select>
           </div>
           <div class="form-group">
             <label for="message">Your Message</label>
-            <textarea class="input-group" v-model="message" name="message"></textarea>
+            <textarea
+              class="input-group"
+              v-model="message"
+              name="message"
+            ></textarea>
           </div>
-          <button type="submit" class="mt-3 btn-submit lg:w-32">SUBMIT</button>
+          <button type="submit" class="mt-3 btn-submit lg:w-32">
+            {{ isLoading ? "Loading" : "SUBMIT" }}
+          </button>
         </form>
       </div>
     </div>
@@ -61,6 +79,7 @@ export default {
       email: "",
       service: "",
       message: "",
+      isLoading: false,
     };
   },
   methods: {
@@ -70,6 +89,8 @@ export default {
       if (!this.service) this.errors.push("Hmmm fill what service you want");
       if (!this.message) this.errors.push("Dont be shy! fill your message ☺️");
       else if (this.name && this.service && this.message) {
+        this.isLoading = true;
+
         axios({
           method: "post",
           url: `https://api.telegram.org/bot${process.env.BOT_TELE_API}/sendMessage`,
@@ -79,6 +100,7 @@ export default {
           },
         })
           .then((res) => {
+            this.isLoading = false;
             this.$router.replace("/success");
           })
           .catch((err) => {
